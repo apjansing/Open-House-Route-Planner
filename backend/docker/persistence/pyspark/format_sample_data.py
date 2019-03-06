bbf26aa85b240cdcca1a10a03ca6dca114b71674
Alexander Jansing <apjansing@alexanders-mbp.lan>
2019-03-06 11:25:52 -0500

    finished python script to process ICS files to dicts, getting geolocation info, and loading to mongodb

--- backend/docker/persistence/pyspark/format_sample_data.py
+++ backend/docker/persistence/pyspark/format_sample_data.py
@@ -1,4 +1,16 @@
-import mongo_ops
+from MongoOps import MongoOps
+from os import listdir
+from os.path import isfile, join
+from pprint import pprint
+from ICSParser import ICSParser
 
-mops = mongo_ops.MongoOps()
-mops.safe_query_for_location_info("415 Van    DyKe Rd., Utica, NY 13502")
+mops = MongoOps()
+
+sample_data_files = [f for f in listdir("/data") if isfile(join("/data", f))]
+i = 0
+for open_house_file in sample_data_files:
+    parser = ICSParser("/data/%s" % open_house_file)
+    event = parser.to_dict()
+    result = mops.safe_query_for_location_info(event)
+    i += 1
+print("%d locations evaluated." % i)

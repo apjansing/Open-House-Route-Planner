0975a129085c3ce7ebd954e5ae3f559a5131f5e6
apjansing <apjansing@gmail.com>
2019-03-23 17:57:20 -0400

    made an 'simplified' directions matrix. It's a little ugly right now, but it has the infomation I need to start figuring out the routes

--- backend/docker/persistence/pyspark/make_directions_matrix.py
+++ backend/docker/persistence/pyspark/make_directions_matrix.py
@@ -10,6 +10,7 @@ class DirectionsMatrix():
     def __init__(self, locations = [], mops = MongoOps()):
         self.locations = locations
         self.directions_matrix = None
+        self.mops = mops
     
     def get_directions_matrix(self):
         directions_matrix = []
@@ -17,7 +18,7 @@ class DirectionsMatrix():
             loc1_row = []
             for location2 in locations:
                 if location1['location'] != location2['location']:
-                    loc1_loc2_directions = mops.safe_query_for_directions(start = location1, stop = location2)
+                    loc1_loc2_directions = self.mops.safe_query_for_directions(start = location1, stop = location2)
                     loc1_row += [loc1_loc2_directions]
                 else:
                     loc1_row += [None]
@@ -32,16 +33,28 @@ class DirectionsMatrix():
             pass
         simplified_directions_matrix = []
         for i in range(len(self.directions_matrix)):
-            simplified_directions_row = []
-            for j in range(len(self.directions_matrix[i])):
-                if self.directions_matrix[i][j] == None:
-                    pass
-                    # simplified_directions_row += [[j, -1]]
-                else:
-                    ij_duration = self.directions_matrix[i][j]['directions'][-1]['Duration (min)']
-                    simplified_directions_row += [[j, ij_duration]]
-            simplified_directions_matrix += [simplified_directions_row]
+            row_data = self._get_start_location_info(i)
+            simplified_directions_matrix += [self._get_time_between_points(i, row_data)]
         self.simplified_directions_matrix = np.array(simplified_directions_matrix)
+
+    def _get_start_location_info(self, row):
+        start = ''
+        if self.directions_matrix[row][0] != None:
+            start = self.directions_matrix[row][0]['start']
+        else:
+            start = self.directions_matrix[row][1]['start']
+        return self.mops.safe_query_for_location_info(start)
+
+    def _get_time_between_points(self, row, simplified_directions_row):
+        durations = []
+        for j in range(len(self.directions_matrix[row])):
+            if self.directions_matrix[row][j] == None:
+                durations += [[j, -1]]
+            else:
+                duration = self.directions_matrix[row][j]['directions'][-1]['Duration (min)']
+                durations += [[j, duration]]
+        simplified_directions_row['durations'] = durations
+        return simplified_directions_row
 
 
 if __name__ == "__main__":
@@ -51,12 +64,13 @@ if __name__ == "__main__":
     for open_house_file in sample_data_files:
         parser = ICSParser("/data/%s" % open_house_file)
         event = parser.to_dict()
+        # print(event)
         result = mops.safe_query_for_location_info(event)
         locations += [result]
     dir_mx = DirectionsMatrix(locations, mops)
     # dir_mx.get_directions_matrix()
     dir_mx.generate_simplified_directions_matrix()
-    print(dir_mx.directions_matrix.shape)
-    print(dir_mx.directions_matrix[0][1])
+    # print(dir_mx.directions_matrix[0][1])
+    # print(dir_mx.directions_matrix.shape)
     print(dir_mx.simplified_directions_matrix)
     
